b1c0f93f22dd18ee14e3756d6f9f72a5295c7a1d
PlutoPenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2023-04-13 00:42:02 -0400

    update()

--- QuikFinance/src/main/java/com/example/quikfinance/QuikFinanceController.java
+++ QuikFinance/src/main/java/com/example/quikfinance/QuikFinanceController.java
@@ -114,7 +114,7 @@ public class QuikFinanceController {
     private Transaction transaction6 = new Transaction();
     private Transaction transaction7 = new Transaction();
     private Transaction transaction8 = new Transaction();
-    //private Storage = Storage.instance();
+    private Storage storage = Storage.instance();
 
     // Put these eight transaction objects into an array.
     private Transaction[] transactions = {transaction1, transaction2, transaction3, transaction4, transaction5, transaction6, transaction7, transaction8};
@@ -341,8 +341,9 @@ public class QuikFinanceController {
             BalanceTextField8.setText(formattedEighthRunningBalance);
     }
 
-    private void update () {
-
+    private void update() {
+        storage.updateAll(transactions);
+        storage.serialize();
     }
 
     // Still needed:
--- QuikFinance/src/main/java/com/example/quikfinance/Storage.java
+++ QuikFinance/src/main/java/com/example/quikfinance/Storage.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import com.example.quikfinance.Transaction;
 
 public class Storage {
 
