45d64f0c6f53aa43e699d95d3e8c6f2c740cdcc1
PlutoPenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2023-04-18 09:06:50 -0400

    Revert "Remove Category attribute from Transaction.java"
    This reverts commit 9bdab88f47a5c2b977ef0d82701ee3931018625e.

--- QuikFinance/src/main/java/com/example/quikfinance/Transaction.java
+++ QuikFinance/src/main/java/com/example/quikfinance/Transaction.java
@@ -5,14 +5,27 @@ import java.io.Serializable;
 public class Transaction implements Serializable {
     private String date;
     private String description;
+    // private Category category;
     private boolean wasPaid;
     protected double amount;
 
     public Transaction() {
         this.date = "";
         this.description = "";
+        // this.category = null;
         this.wasPaid = true;
         this.amount = 0;
+
+        // category.updatePrice((int) amount);
+    }
+    public Transaction(String date, String description, Category category, boolean wasPaid, int amount) {
+        this.date = date;
+        this.description = description;
+        // this.category = category;
+        this.wasPaid = wasPaid;
+        this.amount = amount;
+
+        category.updatePrice(amount);
     }
 
     // Methods to set and get the date of the transaction.
@@ -29,6 +42,15 @@ public class Transaction implements Serializable {
     }
     public String getDescription() {
         return description;
+    }
+
+    // Methods to set and get the category of the transaction.
+    public void setCategory(Category newCategory) {
+        // category = newCategory;
+    }
+    public Category getCategory() {
+        // return category;
+        return null;
     }
 
     // Methods to set and get the boolean that tells us whether the amount was paid or received.
